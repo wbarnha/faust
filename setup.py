bac845a96668c8f61b8ac8771fd34c59d073d583
William Barnhart <williambbarnhart@gmail.com>
2022-09-22 10:33:15 -0400

    test commit for release

--- setup.py
+++ setup.py
@@ -181,7 +181,7 @@ with open("README.md") as readme_file:
 
 def do_setup(**kwargs):
     setup(
-        name="faust-streaming",
+        name="wbarnha-faust-streaming",
         version=meta["version"],
         description=meta["doc"],
         long_description=long_description,
